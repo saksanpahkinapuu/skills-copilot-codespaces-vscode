cb34fe5df9709e7006a5a3bbff94c5de17fecb8b
saksanpahkinapuu <139323560+saksanpahkinapuu@users.noreply.github.com>
2023-09-25 14:09:29 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 4000
+    };
+    console.log(member.skills);
+    console.log(member.skills[1]);
+    console.log(member.skills[2]);
+    console.log(member.skills[0]);
+    console.log(member.skills[3]);
+}
